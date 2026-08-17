82a60dc8e432fe312e387df1d4fc227cb98a35a3
MagbanuaJustin <magbanuaactivity@gmail.com>
2026-08-17 20:26:02 +0800

    Update Welcome Page

--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -1,7 +1,7 @@
 <?php
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
-class StudentController extends Controller {
+class Welcome extends Controller {
 	public function index() {
 		$this->call->view('welcome_page');
 	}
