055ce7aaa02c2fc3bd5219c9f2902241aa55ae53
MagbanuaJustin <magbanuaactivity@gmail.com>
2026-08-17 20:49:58 +0800

    Update LavaLust

--- app/views/StudentProfile.php
+++ app/views/StudentProfile.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>JJ.dev | Student Profile</title>
+<title>Super Student Profile</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
   :root{
